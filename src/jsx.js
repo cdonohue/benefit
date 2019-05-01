7c60c98b1609e4674afcd2e2658a97501f35fb8a
Chad Donohue <chad.donohue@gmail.com>
2019-04-30 23:12:20 -0500

    Update node version and rename jsx pragma

--- src/jsx.js
+++ src/jsx.js
@@ -1,13 +1,10 @@
-import { jsx as jsxEmotion } from "emotion"
 import React from "react"
 import Box from "./components/Box"
 
-function jsx(type, props, ...children) {
+export default function jsx(type, props, ...children) {
   return (
     <Box is={type} {...props}>
       {children}
     </Box>
   )
 }
-
-export default jsx
