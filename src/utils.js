0c03b5a972f4d123a143dbe547ed72e58fc2415f
Chad Donohue <chad.donohue@gmail.com>
2019-05-03 11:43:40 -0500

    All named exports

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export {
-  default as createUtilitiesFromMap,
-} from "./config/createUtilitiesFromMap"
-export { default as createVariant } from "./config/createVariant"
-export {
-  default as createVariantsFromMap,
-} from "./config/createVariantsFromMap"
-export { default as flattenColorConfig } from "./config/flattenColorConfig"
