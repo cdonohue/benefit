0e97e27a48d4517a0f8de781f380b02c07ea4f36
Chad Donohue <chad.donohue@gmail.com>
2019-05-03 10:50:39 -0500

    Separate default export from utility functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import createUtilitiesFromConfig from "./createUtilitiesFromConfig"
 
 export default createUtilitiesFromConfig
-
-export {
-  default as createUtilitiesFromMap,
-} from "./config/createUtilitiesFromMap"
-export { default as createVariant } from "./config/createVariant"
-export {
-  default as createVariantsFromMap,
-} from "./config/createVariantsFromMap"
-export { default as flattenColorConfig } from "./config/flattenColorConfig"
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export {
+  default as createUtilitiesFromMap,
+} from "./config/createUtilitiesFromMap"
+export { default as createVariant } from "./config/createVariant"
+export {
+  default as createVariantsFromMap,
+} from "./config/createVariantsFromMap"
+export { default as flattenColorConfig } from "./config/flattenColorConfig"
