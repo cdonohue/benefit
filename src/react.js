e380e528cd636f374968db5714f239caae3b9791
Eric Clemmons <eric@smarterspam.com>
2019-05-05 18:18:57 -0700

    Split { Box, ConfigProvider, ConfigConsumer, jsx } to "benefit/react"

--- src/react.js
+++ src/react.js
@@ -11,3 +11,12 @@ export {
 } from "./config/createVariantsFromMap"
 
 export { default as flattenColorConfig } from "./config/flattenColorConfig"
+
+export { default as Box } from "./components/Box"
+
+export {
+  ConfigProvider,
+  ConfigConsumer,
+} from "./components/ConfigContext/index"
+
+export { default as jsx } from "./jsx"
