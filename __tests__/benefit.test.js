70dfdb010fa248105e200a1aeb7b303ba07a6082
Eric Clemmons <eric@smarterspam.com>
2019-05-14 21:30:47 -0700

    Add benefit snapshot

---
+++ __tests__/benefit.test.js
@@ -0,0 +1,19 @@
+const benefit = require("../dist/benefit")()
+
+describe("benefit()", () => {
+  it("should match snapshot of exports", async () => {
+    expect(Object.keys(benefit)).toMatchInlineSnapshot(`
+      Array [
+        "config",
+        "utilities",
+        "styleWith",
+      ]
+    `)
+  })
+
+  describe("utilities", () => {
+    it("should match snapshot", () => {
+      expect(benefit.utilities).toMatchSnapshot()
+    })
+  })
+})
