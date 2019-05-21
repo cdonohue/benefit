0f69f3e6de47e5c1048bbbf84042436de2a20f82
Chad Donohue <chad.donohue@gmail.com>
2019-05-21 11:12:52 -0500

    Update descriptions and keywords

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,6 @@
 export default {
-  title: "<Benefit />",
-  description: "Benefit documentation",
+  title: "benefit",
+  description:
+    "5kB utility CSS framework that's fully compatible with TailwindCSS",
   theme: "docs/theme/index",
 }
