842edccf84c8ec9bcd0fe903abad9f4fdee281d8
Chad Donohue <chad.donohue@gmail.com>
2019-05-22 09:59:50 -0500

    Update docs description

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,5 @@
 export default {
   title: "benefit",
-  description:
-    "5kB utility CSS framework that's fully compatible with TailwindCSS",
+  description: "5kB utility CSS framework that is compatible with TailwindCSS",
   theme: "docs/theme/index",
 }
