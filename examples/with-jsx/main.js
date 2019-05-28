3a706e21a4163f8770dfffbee232bf08e5ea4d5b
Chad Donohue <chad.donohue@gmail.com>
2019-05-27 21:59:08 -0500

    Update example

--- examples/with-jsx/main.js
+++ examples/with-jsx/main.js
@@ -6,14 +6,27 @@ import { jsx } from "../../dist/react"
 
 function App() {
   return (
-    <div
-      css={css`
-        font-size: 64px;
-        font-weight: bold;
-      `}
-      className="bg-blue-300"
-    >
-      Hello
+    <div className="font-bold rounded overflow-hidden shadow-lg max-w-xl">
+      <div
+        css={css`
+          background: linear-gradient(to right, indianred, firebrick);
+        `}
+        className="bg-blue-300 p-8 relative h-64"
+      >
+        <h1 className="absolute bottom-0 text-white text-6xl -mb-5">
+          Dolor Sit Amet
+        </h1>
+      </div>
+      <div className="p-8 pt-0">
+        <h1
+          css={css`
+            color: indianred;
+          `}
+          className="text-6xl"
+        >
+          Lorem Ipsum
+        </h1>
+      </div>
     </div>
   )
 }
