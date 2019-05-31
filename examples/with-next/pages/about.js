6d61ccdbeda3155ce00b82c4f2e90508203c3eeb
Chad Donohue <chad.donohue@gmail.com>
2019-05-31 18:59:14 -0500

    Link back to index page

--- examples/with-next/pages/about.js
+++ examples/with-next/pages/about.js
@@ -1,7 +1,10 @@
 import { jsx } from "../../../dist/react"
+import Link from 'next/link'
 
 function About() {
-  return <div className="font-bold text-primary">Welcome to About</div>
+  return <div className="font-bold text-blue-600">Welcome to About <Link href="/">
+  <a className="cursor-pointer font-bold text-red-500">Back Home</a>
+</Link>{' '}</div>
 }
 
 export default About
