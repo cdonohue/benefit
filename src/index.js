8d546408c13aaf6a2bbbb9c4a201bdacaf51392a
Chad Donohue <chad.donohue@gmail.com>
2019-06-02 08:14:17 -0500

    Add on styleWith and cx to vanilla export

--- src/index.js
+++ src/index.js
@@ -2,16 +2,42 @@ import { css, cx } from "emotion"
 
 import createUtilitiesFromConfig from "./initialization/createUtilitiesFromConfig"
 
-export default function benefit(config) {
-  return createUtilitiesFromConfig(config, {
-    processDeclarationFn: (declaration) => css`
-      ${declaration}
-    `,
-    cxFn: (styleWith, ...args) => {
+export default function benefit(configFn) {
+  const {
+    config,
+    cssForUtility,
+    getDeclarationsForClasses,
+    utilities,
+  } = createUtilitiesFromConfig(configFn)
+
+  const styleWith = (classNames = "", isImportant = false) => {
+    const { declarations, ignoredClasses } = getDeclarationsForClasses(
+      classNames,
+      isImportant
+    )
+
+    return [
+      ...declarations.map(
+        (declaration) =>
+          css`
+            ${declaration}
+          `
+      ),
+      ...ignoredClasses,
+    ].join(" ")
+  }
+
+  return {
+    config,
+    cssForUtility,
+    cx(...args) {
       const appliedClasses = cx(...args)
       const styledClasses = styleWith(appliedClasses)
 
       return cx(styledClasses)
     },
-  })
+    getDeclarationsForClasses,
+    styleWith,
+    utilities,
+  }
 }
