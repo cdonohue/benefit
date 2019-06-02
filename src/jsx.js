d5e66994f98f8f9140f105fa662142865ec7ecbe
Chad Donohue <chad.donohue@gmail.com>
2019-06-01 19:02:00 -0500

    Only pass components through Box if they have css or className

--- src/jsx.js
+++ src/jsx.js
@@ -3,6 +3,12 @@ import { jsx } from "@emotion/core"
 import Box from "./components/Box"
 
 export default function h(type, props, ...children) {
+  const { className = null, css = null } = props
+
+  if (!className && !css) {
+    return React.createElement(type, props, ...children)
+  }
+
   return (
     <Box h={jsx} is={type} {...props}>
       {children}
