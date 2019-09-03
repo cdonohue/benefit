f609d247e511807a5acba6cd7f9e651c35b3a70e
Chad Donohue <chad.donohue@gmail.com>
2019-09-03 08:42:27 -0500

    export registry

--- packages/benefit/index.ts
+++ packages/benefit/index.ts
@@ -4,6 +4,7 @@ import createVariant from "./config/createVariant"
 import createVariantsFromMap from "./config/createVariantsFromMap"
 import { css, injectGlobal, keyframes } from "./util/css"
 import { preflightStyles } from "./util/preflight"
+import registry from "./util/registry"
 import baseStyles from "./util/baseStyles"
 import flattenColorConfig from "./config/flattenColorConfig"
 import parseDeclarations from "./util/parseDeclarations"
@@ -21,5 +22,6 @@ export {
   keyframes,
   parseDeclarations,
   preflightStyles,
+  registry,
   theme,
 }
