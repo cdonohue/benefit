2665ef56299c36d455e90f941289a74ed95b9659
Chad Donohue <chad.donohue@gmail.com>
2019-09-03 13:16:37 -0500

    Refactor styles container

--- packages/benefit-react/StylesContainer.tsx
+++ packages/benefit-react/StylesContainer.tsx
@@ -1,118 +1,52 @@
 import React from "react"
-import { ConfigConsumer } from "./ConfigContext"
+import { registry } from "benefit"
 
-function renderStyleContainers(props: any) {
+function createContainer(label: string, styles: any[]) {
   return (
-    <ConfigConsumer>
-      {({ benefitRegistry }) => {
-        const {
-          css,
-          global,
-          keyframes,
-          preflight,
-          utilities,
-        } = benefitRegistry.getRegistry()
-
-        return (
-          <div id="benefit-container">
-            {!props.isBrowser && (
-              <script
-                {...{
-                  dangerouslySetInnerHTML: {
-                    __html: `window.benefit = ${JSON.stringify(
-                      benefitRegistry.getRegistry()
-                    )}`,
-                  },
-                }}
-              ></script>
-            )}
-            <div id="benefit-preflight">
-              {preflight.map((s: any) => (
-                <style
-                  data-benefit-preflight
-                  {...{
-                    id: s.id,
-                    key: `${s.id}`,
-                    dangerouslySetInnerHTML: {
-                      __html: s.rules,
-                    },
-                  }}
-                ></style>
-              ))}
-            </div>
-            <div id="benefit-global">
-              {global.map((s: any) => (
-                <style
-                  data-benefit-global
-                  {...{
-                    id: s.id,
-                    key: `${s.id}`,
-                    dangerouslySetInnerHTML: {
-                      __html: s.rules,
-                    },
-                  }}
-                ></style>
-              ))}
-            </div>
-            <div id="benefit-keyframes">
-              {keyframes.map((s: any) => (
-                <style
-                  data-benefit-keyframes
-                  {...{
-                    id: s.id,
-                    key: `${s.id}`,
-                    dangerouslySetInnerHTML: {
-                      __html: s.rules,
-                    },
-                  }}
-                ></style>
-              ))}
-            </div>
-            <div id="benefit-utilities">
-              {utilities.map((s: any) => (
-                <style
-                  data-benefit-utility
-                  data-benefit-index={s.index}
-                  {...{
-                    id: s.id,
-                    key: `${s.id}`,
-                    dangerouslySetInnerHTML: {
-                      __html: s.rules,
-                    },
-                  }}
-                ></style>
-              ))}
-            </div>
-            <div id="benefit-css">
-              {css.map((s: any) => (
-                <style
-                  data-benefit-css
-                  {...{
-                    id: s.id,
-                    key: `${s.id}`,
-                    dangerouslySetInnerHTML: {
-                      __html: s.rules,
-                    },
-                  }}
-                ></style>
-              ))}
-            </div>
-          </div>
-        )
-      }}
-    </ConfigConsumer>
+    <div id={`benefit-${label}`}>
+      {styles.map((s: any) => (
+        <style
+          {...{
+            ...(s.index && { "data-benefit-index": s.index }),
+            [`data-benefit-${label}`]: true,
+            id: s.id,
+            key: `${s.id}`,
+            dangerouslySetInnerHTML: {
+              __html: s.rules,
+            },
+          }}
+        ></style>
+      ))}
+    </div>
   )
 }
 
 export default function StylesContainer() {
-  const isBrowser = typeof window !== "undefined"
+  const benefitRegistry = registry.getInstance()
+  const {
+    css,
+    global,
+    keyframes,
+    preflight,
+    utilities,
+  } = benefitRegistry.getRegistry()
 
-  if (
-    !isBrowser ||
-    (isBrowser && !document.getElementById("benefit-container"))
-  ) {
-    return renderStyleContainers(isBrowser)
-  }
-
-  return null
+  return (
+    <div id="benefit-container">
+      <script
+        {...{
+          dangerouslySetInnerHTML: {
+            __html: `window.benefit = ${JSON.stringify(
+              benefitRegistry.getRegistry()
+            )}`,
+          },
+        }}
+      ></script>
+      {createContainer("preflight", preflight)}
+      {createContainer("global", global)}
+      {createContainer("keyframes", keyframes)}
+      {createContainer("utilities", utilities)}
+      {createContainer("css", css)}
+    </div>
+  )
 }
